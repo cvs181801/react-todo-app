701c89e4795c4f60001f355e4a21a40e245c8bf3
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-09-29 16:56:08 -0700

    separate out the checkboxes into a new functional component

--- src/components/Checkboxes.js
+++ src/components/Checkboxes.js
@@ -1,17 +1,10 @@
 import React from 'react'
+import IndividualCheckboxes from "./IndividualCheckboxes"
 
 export default function Checkboxes() {
     return (
         <div className="ToDos-Div">
-            <label>Laundry</label>
-            <input type="checkbox" ></input>
-            <p>Excellent</p>
-            <label>Dishes</label>
-            <input id="checkbox2" type="checkbox"></input>
-            <p>Excellent</p>
-            <label>Relax</label>
-            <input id="checkbox3" type="checkbox"></input>
-            <p>Excellent</p>
+           < IndividualCheckboxes />
         </div>
     )
 }
---
+++ src/components/IndividualCheckboxes.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function IndividualCheckboxes() {
+    return(
+        <div className="indivCheckboxes">
+            <label>Laundry</label>
+            <input type="checkbox" ></input>
+            <p>Excellent</p>
+            <label>Dishes</label>
+            <input id="checkbox2" type="checkbox"></input>
+            <p>Excellent</p>
+            <label>Relax</label>
+            <input id="checkbox3" type="checkbox"></input>
+            <p>Excellent</p>
+            </div>
+    )
+}
