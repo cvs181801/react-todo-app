387e47e243ca0db65ab909b67fd90d0fcb73b3b2
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-09-28 18:22:06 -0700

    initial commit

--- src/components/Checkboxes.js
+++ src/components/Checkboxes.js
@@ -5,11 +5,13 @@ export default function Checkboxes() {
         <div>
             <label>Laundry</label>
             <input id="checkbox1" type="checkbox" ></input>
+            <p>Excellent</p>
             <label>Dishes</label>
             <input id="checkbox2" type="checkbox"></input>
+            <p>Excellent</p>
             <label>Relax</label>
             <input id="checkbox3" type="checkbox"></input>
-       
+            <p>Excellent</p>
         </div>
     )
 }
