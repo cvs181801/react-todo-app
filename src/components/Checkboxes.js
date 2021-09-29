f0edcd38c10801de8833180bfac810c336e432cb
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-09-28 18:19:15 -0700

    initial commit

---
+++ src/components/Checkboxes.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Checkboxes() {
+    return (
+        <div>
+            <label>Laundry</label>
+            <input id="checkbox1" type="checkbox" ></input>
+            <label>Dishes</label>
+            <input id="checkbox2" type="checkbox"></input>
+            <label>Relax</label>
+            <input id="checkbox3" type="checkbox"></input>
+       
+        </div>
+    )
+}
